a7e10cb226201e5b942e20abf1dec197da93150a
jeyji1949 <96532921+jeyji1949@users.noreply.github.com>
2025-03-26 14:47:00 +0000

    Add files via upload

---
+++ JihaneWeatherAppTests/JihaneWeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JihaneWeatherAppTests.swift
+//  JihaneWeatherAppTests
+//
+//  Created by User on 25/03/2025.
+//
+
+import Testing
+@testable import JihaneWeatherApp
+
+struct JihaneWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
